89538bc63f952fe4238ca06f8f37533ceb51eb31
Alex Ilushyk <ailushyk@gmail.com>
2024-06-09 19:07:31 +0200

    init commit

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import createError from 'http-errors'
+import homRouter from '@/hom/api/hom-router'
+
+import { errorHandler } from '@/utils/error-handler'
+
+const app = express()
+
+app.use(express.json())
+app.use('/', homRouter)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+app.use(errorHandler)
+
+export default app
